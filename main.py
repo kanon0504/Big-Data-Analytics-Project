1bc32601c806248011e6f1b71738f7ac6d708533
shixiaohan <xiaohan.shi@live.com>
2015-12-06 21:21:56 +0100

    I split the train data in to X_train and X_test, and I try Naive Bayes Model, SVM Model and Logistic Regression Model and get their rate(score)

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pyspark import SparkContext
-import loadFiles as lf
+import LoadFiles as lf
 import numpy as np
 from random import randint
 from  pyspark.mllib.classification import NaiveBayes
 from functools import partial
 from pyspark.mllib.linalg import SparseVector
 from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.classification import SVMWithSGD
+from pyspark.mllib.classification import LogisticRegressionWithSGD
+
 sc = SparkContext(appName="Simple App")
 
 
@@ -18,14 +21,29 @@ def createBinaryLabeledPoint(doc_class,dictionary):
 	for w in words:
 		vector_dict[dictionary[w]]=1
 	return LabeledPoint(doc_class[1], SparseVector(len(dictionary),vector_dict))
+
+# I change the function "predict" a little, because I use the "test data", created randomly with bernouille distribution from our train data, as my input, not the test data given by the professor.
 def Predict(name_text,dictionary,model):
-	words=name_text[1].strip().split(' ')
+	words=name_text[0].strip().split(' ')
 	vector_dict={}
 	for w in words:
 		if(w in dictionary):
 			vector_dict[dictionary[w]]=1
-	return (name_text[0], model.predict(SparseVector(len(dictionary),vector_dict)))
-data,Y=lf.loadLabeled("/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/train")
+	return (name_text[1], model.predict(SparseVector(len(dictionary),vector_dict)))
+
+#data,Y=lf.loadLabeled("/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/train")
+data,Y=lf.loadLabeled("data/train")
+
+
+#divide train data into X_test and X_train, divide the label into y_train and y_test
+flag = np.random.binomial(1, 0.75, len(data))
+X_train=[data[i] for i in range(len(data)) if flag[i]]
+y_train=[Y[i] for i in range(len(data)) if flag[i]]
+X_test=[data[i] for i in range(len(data)) if not flag[i]]
+y_test=[Y[i] for i in range(len(data)) if not flag[i]]
+
+
+
 print len(data)
 dataRDD=sc.parallelize(data,numSlices=16)
 #map data to a binary matrix
@@ -49,11 +67,63 @@ data_class=zip(data,Y)#if a=[1,2,3] & b=['a','b','c'] then zip(a,b)=[(1,'a'),(2,
 dcRDD=sc.parallelize(data_class,numSlices=16)
 #get the labelled points
 labeledRDD=dcRDD.map(partial(createBinaryLabeledPoint,dictionary=dict_broad.value))
-#Train NaiveBayes
-model=NaiveBayes.train(labeledRDD)
-#broadcast the model
-mb=sc.broadcast(model)
 
+
+
+
+
+# NaiveBayes Model
+modelNB=NaiveBayes.train(labeledRDD)
+#broadcast the model
+mbNB=sc.broadcast(modelNB)
+name_text=zip(X_test, y_test)
+#for each doc :(name,text):
+#apply the model on the vector representation of the text
+#return the name and the class
+predictionsNB=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbNB.value)).collect()
+#rate 0.8857142857142857
+rateNB = sum([1.0 for i in range(len(predictionsNB)) if predictionsNB[i][0] == predictionsNB[i][1]])/len(predictionsNB)
+print rateNB
+#0.8857142857142857
+
+
+
+#SVM Model
+modelSVM=SVMWithSGD.train(labeledRDD)
+#broadcast the model
+mbSVM=sc.broadcast(modelSVM)
+name_text=zip(X_test, y_test)
+#for each doc :(name,text):
+#apply the model on the vector representation of the text
+#return the name and the class
+predictionsSVM=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbSVM.value)).collect()
+#rate
+rateSVM = sum(1.0 for i in range(len(predictionsSVM)) if predictionsSVM[i][0] == predictionsSVM[i][1])/len(predictionsSVM)
+print rateSVM
+#0.819047619047619
+
+
+
+
+#Logistic Regression Model
+modelLR=LogisticRegressionWithSGD.train(labeledRDD)
+#broadcast the model
+mbLR=sc.broadcast(modelLR)
+name_text=zip(X_test, y_test)
+#for each doc :(name,text):
+#apply the model on the vector representation of the text
+#return the name and the class
+predictionsLR=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbLR.value)).collect()
+#rate 0.8058252427184466
+rateLR = sum(1.0 for i in range(len(predictionsLR)) if predictionsLR[i][0] == predictionsLR[i][1])/len(predictionsLR)
+print rateLR
+#0.8058252427184466
+
+
+
+
+# We do this later, after picking up a good classifier...
+'''
 test,names=lf.loadUknown('/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/test')
 name_text=zip(names,test)
 #for each doc :(name,text):
@@ -65,7 +135,7 @@ output=file('./classifications.txt','w')
 for x in predictions:
 	output.write('%s\t%d\n'%x)
 output.close()
-
+'''
 
 
 
