9d3f4d17194bdaa5ba1e3716fa6508b4aae505ac
shixiaohan <xiaohan.shi@live.com>
2015-12-10 14:44:01 +0100

    local change

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from pyspark import SparkContext
-<<<<<<< HEAD
 import LoadFiles as lf
-=======
-import loadFiles_v1 as lf
->>>>>>> 7c5d5208b8490aa882cc90b09715592f46407311
 import numpy as np
 from random import randint
 from  pyspark.mllib.classification import NaiveBayes
 from functools import partial
 from pyspark.mllib.linalg import SparseVector
 from pyspark.mllib.regression import LabeledPoint
-<<<<<<< HEAD
+from sklearn import cross_validation
+from sklearn.naive_bayes import GaussianNB
+from nltk.stem import PorterStemmer
 from pyspark.mllib.classification import SVMWithSGD
 from pyspark.mllib.classification import LogisticRegressionWithSGD
 import string
-
-=======
-from sklearn import cross_validation
-from sklearn.naive_bayes import GaussianNB
->>>>>>> 7c5d5208b8490aa882cc90b09715592f46407311
 
 
 sc = SparkContext(appName="Simple App")
@@ -34,32 +27,37 @@ def createBinaryLabeledPoint(doc_class,dictionary):
 		vector_dict[dictionary[w]]=1
 	return LabeledPoint(doc_class[1], SparseVector(len(dictionary),vector_dict))
 
-<<<<<<< HEAD
-# I change the function "predict" a little, because I use the "test data", created randomly with bernouille distribution from our train data, as my input, not the test data given by the professor.
-=======
 def createWeightedLabeledPoint(doc_class,dictionary):
-	words=doc_class[0].strip().split(' ')
+	words=doc_class.strip().split(' ')
 	vector_dict={}
 	for w in words:
 		vector_dict[dictionary[w]]=1
-	return (doc_class[1], np.array(SparseVector(len(dictionary),vector_dict)))
+	return (doc_class, np.array(SparseVector(len(dictionary),vector_dict)))
 
->>>>>>> 7c5d5208b8490aa882cc90b09715592f46407311
 def Predict(name_text,dictionary,model):
-	words=name_text[0].strip().split(' ')
+	words=name_text.strip().split(' ')
 	vector_dict={}
 	for w in words:
 		if(w in dictionary):
 			vector_dict[dictionary[w]]=1
-<<<<<<< HEAD
-	return (name_text[1], model.predict(SparseVector(len(dictionary),vector_dict)))
+	return (name_text, model.predict(SparseVector(len(dictionary),vector_dict)))
 
-#data,Y=lf.loadLabeled("/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/train")
-data,Y=lf.loadLabeled("data/train")
-
-
+def Accuracy(predictions,y):
+	counter = 0.
+	for i in range(len(predictions)):
+		if predictions[i] == y_test[i]:
+			counter += 1.
+	accuracy = counter/len(predictions)
+	return accuracy
+ 
+ 
+ 
+ 
+ 
+############################### Data Load & Preprocessing ###############################
+data_O,Y_O=lf.loadLabeled("data/train")
 #data preprocessing
-data=[w.decode('utf-8') for w in data ]
+data_O=[w.decode('utf-8') for w in data_O ]
 stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
         'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
         'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
@@ -73,7 +71,7 @@ stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
         'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
         'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
         
-for doc_id, text in enumerate(data):
+for doc_id, text in enumerate(data_O):
     
     # Remove punctuation and lowercase
     punctuation = set(string.punctuation)    
@@ -88,42 +86,19 @@ for doc_id, text in enumerate(data):
         
     # Covenrt list of words to one string
     doc = ' '.join(w for w in doc)
-    data[doc_id] = doc  
-
-
-
-
-#divide train data into X_test and X_train, divide the label into y_train and y_test
-flag = np.random.binomial(1, 0.75, len(data))
-X_train=[data[i] for i in range(len(data)) if flag[i]]
-y_train=[Y[i] for i in range(len(data)) if flag[i]]
-X_test=[data[i] for i in range(len(data)) if not flag[i]]
-y_test=[Y[i] for i in range(len(data)) if not flag[i]]
-
-
-
-=======
-	return (name_text[0], model.predict(SparseVector(len(dictionary),vector_dict)))
-
-def Accuracy(predictions,y):
-	counter = 0.
-	for i in range(len(predictions)):
-		if predictions[i] == y_test[i]:
-			counter += 1.
-	accuracy = counter/len(predictions)
-	return accuracy
+    data_O[doc_id] = doc  
+############################### Data Load & Preprocessing ###############################
+ 
+ 
+ 
 
 ############################### Split Dataset ###############################
-data_O,Y_O=lf.loadLabeled("/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/train")
 data,x_test,Y,y_test = cross_validation.train_test_split(data_O, Y_O, test_size=0.2, random_state=0)
 ############################### Split Dataset ###############################
 
 
 ############################# Creat Dictionary #############################
-data,Y=lf.loadLabeled("/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/train")
->>>>>>> 7c5d5208b8490aa882cc90b09715592f46407311
-print len(data)
-dataRDD=sc.parallelize(data,numSlices=16)
+dataRDD=sc.parallelize(data_O,numSlices=16)
 lists=dataRDD.map(lambda x:list(set(x.strip().split(' ')))).collect()
 all=[]
 for l in lists:
@@ -141,85 +116,28 @@ dict_broad=sc.broadcast(dictionary)
 data_class=zip(data,Y)
 dcRDD=sc.parallelize(data_class,numSlices=16)
 labeledRDD=dcRDD.map(partial(createBinaryLabeledPoint,dictionary=dict_broad.value))
-<<<<<<< HEAD
-
-
-
-
-
-# NaiveBayes Model
-modelNB=NaiveBayes.train(labeledRDD)
-#broadcast the model
-mbNB=sc.broadcast(modelNB)
-name_text=zip(X_test, y_test)
-#for each doc :(name,text):
-#apply the model on the vector representation of the text
-#return the name and the class
-predictionsNB=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbNB.value)).collect()
-#rate 0.8857142857142857
-rateNB = sum([1.0 for i in range(len(predictionsNB)) if predictionsNB[i][0] == predictionsNB[i][1]])/len(predictionsNB)
-print 'rateNB',rateNB
-#0.922855773838
-
-
-
-#SVM Model
-modelSVM=SVMWithSGD.train(labeledRDD)
-#broadcast the model
-mbSVM=sc.broadcast(modelSVM)
-name_text=zip(X_test, y_test)
-#for each doc :(name,text):
-#apply the model on the vector representation of the text
-#return the name and the class
-predictionsSVM=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbSVM.value)).collect()
-#rate
-rateSVM = sum(1.0 for i in range(len(predictionsSVM)) if predictionsSVM[i][0] == predictionsSVM[i][1])/len(predictionsSVM)
-print 'rateSVM',rateSVM
-#0.866475003993
-
-
-
-
-#Logistic Regression Model
-modelLR=LogisticRegressionWithSGD.train(labeledRDD)
-#broadcast the model
-mbLR=sc.broadcast(modelLR)
-name_text=zip(X_test, y_test)
-#for each doc :(name,text):
-#apply the model on the vector representation of the text
-#return the name and the class
-predictionsLR=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mbLR.value)).collect()
-#rate 0.8058252427184466
-rateLR = sum(1.0 for i in range(len(predictionsLR)) if predictionsLR[i][0] == predictionsLR[i][1])/len(predictionsLR)
-print 'rateLR',rateLR
-#0.846030985466
-
-
-
-
-# We do this later, after picking up a good classifier...
-'''
-test,names=lf.loadUknown('/Users/Kanon/Documents/X-courses/MAP670/Big Data Analytics Project 2015/data/test')
-name_text=zip(names,test)
-#for each doc :(name,text):
-#apply the model on the vector representation of the text
-#return the name and the class
-predictions=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mb.value)).collect()
-
-output=file('./classifications.txt','w')
-for x in predictions:
-	output.write('%s\t%d\n'%x)
-output.close()
-'''
-=======
 model=NaiveBayes.train(labeledRDD)
 mb=sc.broadcast(model)
 predictions=sc.parallelize(x_test).map(partial(Predict,dictionary=dict_broad.value,model=mb.value)).collect()
 print "Naive_Bayes_Accuracy:",Accuracy([i[1] for i in predictions],y_test)
 ############################### Train NBmodel ###############################
 
->>>>>>> 7c5d5208b8490aa882cc90b09715592f46407311
 
+############################### Train SVM model ###############################
+model_SVM=SVMWithSGD.train(labeledRDD)
+mb_SVM=sc.broadcast(model_SVM)
+predictions_SVM=sc.parallelize(x_test).map(partial(Predict,dictionary=dict_broad.value,model=mb_SVM.value)).collect()
+print "SVM_Accuracy:",Accuracy([i[1] for i in predictions_SVM],y_test)
+############################### Train SVM model ###############################
+
+############################### Train Logistic Regression model ###############################
+model_LR=LogisticRegressionWithSGD.train(labeledRDD)
+mb_LR=sc.broadcast(model_LR)
+predictions_LR=sc.parallelize(x_test).map(partial(Predict,dictionary=dict_broad.value,model=mb_LR.value)).collect()
+print "LR_Accuracy:",Accuracy([i[1] for i in predictions_LR],y_test)
+############################### Train Logistic Regression model ###############################
+
+'''
 ############################### Train Gaussian NBmodel ###############################
 data_class=zip(data,Y)
 dcRDD=sc.parallelize(data_class,numSlices=16)
@@ -228,8 +146,9 @@ model = GaussianNB()
 predictions=model.fit(labeledRDD[1],labeledRDD[0]).predict(x_test)
 print "GaussianNB_Accuracy:",Accuracy(predictions,y_test)
 ############################### Train Gaussian NBmodel ###############################
-
+'''
 
 # Accuracy of given algo: 85.54%
+
 
 
